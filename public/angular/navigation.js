cde305e4370c9f5ba342fc9bbb43f3a1b2f25012
varunon9 <varunon9@gmail.com>
2016-05-09 16:57:52 +0530

    type message added

--- public/angular/navigation.js
+++ public/angular/navigation.js
@@ -7,6 +7,8 @@ var navigation = angular.module('navigation', []);
 navigation.controller('navigationController', function ($scope, $http, $window) {
     var socket = io();
     var username, user;
+    $scope.chatTabs = ['varunon9', 'vk1234'];
+    $scope.typedMessage = {};
     socket.on('connect', function () {
        console.log('connected'); 
     });
@@ -19,6 +21,18 @@ navigation.controller('navigationController', function ($scope, $http, $window) 
     		return el.username != username;
     	});
     	$scope.$apply();
+    });
+    socket.on('chatFromServer', function (chat) {
+    	var username = chat.username;
+    	var message = chat.message;
+    	var index = $scope.chatTabs.indexOf(username);
+    	if (index == -1) {
+    		$scope.chatTabs.push(username);
+    	}
+    	var temp = username + 'Messages';
+    	var para = '<p class="server"'> + message + '</p>';
+    	$scope[temp] = $scope[temp] || [];
+    	$scope[temp].push(para);
     });
     var reqUserDetails = {
     	method: 'POST',
@@ -51,6 +65,23 @@ navigation.controller('navigationController', function ($scope, $http, $window) 
     	console.log('error in getting allOnlineUsers: ' + response);
     });
     $scope.connect = function (index) {
-    	//console.log($scope.onlineUsers[index]);
+    	$scope.chatTabs.push($scope.onlineUsers[index].username);
+    };
+    $scope.disconnect = function (index) {
+    	$scope.chatTabs.splice(index, 1);
+    };
+    $scope.sendMessage = function (index) {
+    	var message = $scope.typedMessage.value;
+    	$scope.typedMessage.value = '';
+    	var username = $scope.chatTabs[index];
+    	var chat = {
+    		'username': username,
+    		'message': message
+    	};
+    	socket.emit('chatFromClient', chat);
+    	var temp = username + 'Messages';
+    	var para = '<p class="client">' + message + '</p>';
+    	$scope[temp] = $scope[temp] || [];
+    	$scope[temp].push(para);
     };
 });
